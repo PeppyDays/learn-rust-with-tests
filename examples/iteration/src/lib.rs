b1f4325675d987f214c07f1ea4cfd85fefbc87b6
Arine <peppydays@gmail.com>
2025-06-02 01:15:19 +0900

    chore: remove redundant code in lib.rs

--- examples/iteration/src/lib.rs
+++ examples/iteration/src/lib.rs
@@ -1,31 +1,3 @@
 pub mod v1;
 pub mod v2;
 pub mod v3;
-
-const REPEAT_COUNT: usize = 10;
-
-pub fn repeat(c: &str) -> String {
-    let mut repeated = String::with_capacity(REPEAT_COUNT * c.len());
-    for _ in 0..REPEAT_COUNT {
-        repeated.push_str(c);
-    }
-    repeated
-}
-
-#[cfg(test)]
-mod specs_for_repeat {
-    use super::repeat;
-
-    #[test]
-    fn sut_repeats_given_argument_10_times() {
-        // Arrange
-        let input = "a";
-
-        // Act
-        let actual = repeat(input);
-
-        // Assert
-        let expected = "aaaaaaaaaa";
-        assert_eq!(expected, actual);
-    }
-}
